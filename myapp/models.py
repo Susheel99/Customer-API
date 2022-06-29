27271dc8ca95296d8671dab9a6b6ac1ca9e8dd74
Susheel Arukonda <asusheel23b@gmail.com>
2022-06-29 19:50:02 +0530

    Created DB Models

---
+++ myapp/models.py
@@ -0,0 +1,31 @@
+import email
+from tkinter import CASCADE
+from django.db import models
+
+
+# Create your models here.
+
+class Customer(models.Model):
+    #customer_id = models.CharField(max_length=6, primary_key=True, default=pkgen)
+    customer_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    mobile_number = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+
+class Order(models.Model):
+    customer_id = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    pricing = models.IntegerField()
+    mrp = models.IntegerField()
+
+class Shipping(models.Model):
+    customer_id = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=50)
+
+
+
+
+    
